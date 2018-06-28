3b898fee30bbf95a6e1025c3533e184fc07f01a6
mason <zfixer@163.com>
2018-06-28 16:12:12 +0800

    update

--- StatisticalSummary.py
+++ StatisticalSummary.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import pandas as pd
-
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
+from matplotlib import cm
 
 fruits = pd.read_table('fruit_data_with_colors.txt')
 X = fruits[['mass', 'width', 'height', 'color_score']]
@@ -23,3 +24,14 @@ print('Accuracy of Logistic regression classifier on training set: {:.2f}'
      .format(logreg.score(X_train, y_train)))
 print('Accuracy of Logistic regression classifier on test set: {:.2f}'
      .format(logreg.score(X_test, y_test)))
+
+
+
+
+feature_names = ['mass', 'width', 'height', 'color_score']
+X = fruits[feature_names]
+y = fruits['fruit_label']
+cmap = cm.get_cmap('gnuplot')
+scatter = pd.scatter_matrix(X, c = y, marker = 'o', s=40, hist_kwds={'bins':15}, figsize=(9,9), cmap = cmap)
+plt.suptitle('Scatter-matrix for each input variable')
+plt.savefig('fruits_scatter_matrix')
