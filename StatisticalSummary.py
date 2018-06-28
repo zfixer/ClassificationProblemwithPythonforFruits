cc3381f6d8b75e15ee7444227e6c028b83c064c4
mason <zfixer@163.com>
2018-06-28 16:00:47 +0800

    update

--- StatisticalSummary.py
+++ StatisticalSummary.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 
+fruits = pd.read_table('fruit_data_with_colors.txt')
+X = fruits[['mass', 'width', 'height', 'color_score']]
+y = fruits['fruit_label']
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 scaler = MinMaxScaler()
